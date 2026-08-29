950ec1cfe275464c3a0d932c39f25ddbb3b3cb20
Farrukh-Front-Dev <farrukh.front.dev@gmail.com>
2026-08-29 12:11:48 +0300

    feat: make Waybar toggle instantaneous using SIGUSR1

--- hyprland-pro-setup/configs/waybar/toggle.sh
+++ hyprland-pro-setup/configs/waybar/toggle.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-#  _____                 _       __        __          _
-# |_   _|__   __ _  __ _| | ___  \ \      / /_ _ _   _| |__   __ _ _ __
-#   | |/ _ \ / _` |/ _` | |/ _ \  \ \ /\ / / _` | | | | '_ \ / _` | '__|
-#   | | (_) | (_| | (_| | |  __/   \ V  V / (_| | |_| | |_) | (_| | |
-#   |_|\___/ \__, |\__, |_|\___|    \_/\_/ \__,_|\__, |_.__/ \__,_|_|
-#            |___/ |___/                         |___/
-#
 
-if [ -f $HOME/.config/ml4w/settings/waybar-disabled ]; then
-    rm $HOME/.config/ml4w/settings/waybar-disabled
-else
-    touch $HOME/.config/ml4w/settings/waybar-disabled
-fi
-$HOME/.config/waybar/launch.sh &
+# Use native Waybar toggle (SIGUSR1) for instant hide/show
+# This is much faster than killing and restarting Waybar.
+killall -SIGUSR1 waybar
